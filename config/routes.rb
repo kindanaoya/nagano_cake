6aac963e12d3a02d27e2404a6f5e31ef0a2e540d
NaoyaKinda <110654094+kindanaoya@users.noreply.github.com>
2022-09-16 10:48:50 +0000

    [Add]admin/routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,5 @@
 Rails.application.routes.draw do
 
-  namespace :admin do
-    get 'order_details/update'
-  end
-  namespace :admin do
-    get 'orders/show'
-    get 'orders/update'
-  end
-  namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/update'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
-  namespace :admin do
-    get 'items/index'
-    get 'items/new'
-    get 'items/create'
-    get 'items/show'
-    get 'items/edit'
-    get 'items/update'
-  end
   devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
@@ -32,5 +8,14 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
+
+namespace :admin do
+  resources :homes, only: [:top]
+  resources :items, only: [:index, :new, :create, :show, :edit, :update]
+  resources :genres, only: [:index, :create, :edit, :update]
+  resources :customers, only: [:index, :show, :edit, :update]
+  resources :orders, only: [:show, :update]
+  resources :order_details, only: [:update]
+end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
